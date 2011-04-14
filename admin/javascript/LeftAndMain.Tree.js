fc197c922b53ee216b43123a269e666a1e59002a
Ingo Schommer <ingo@silverstripe.com>
2011-04-15 10:34:48 +1200

    MINOR Hardcoding jstree CSS url to avoid loading non-existant CSS files via ajax (through jstree library)

--- admin/javascript/LeftAndMain.Tree.js
+++ admin/javascript/LeftAndMain.Tree.js
@@ -71,7 +71,8 @@
 								"drag_target" : false
 							},
 							'themes': {
-								'theme': 'apple'
+								'theme': 'apple',
+								'url': 'sapphire/thirdparty/jstree/themes/apple/style.css'
 							},
 							// 'plugins': ['html_data', 'ui', 'dnd', 'crrm', 'themeroller']
 							'plugins': [
